bb0ed2aa5b80e73d0114d6f16a0fd622e7e582a2
TestLeaf <babu@testleaf.com>
2023-08-07 18:45:44 +0530

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,17 +2,26 @@
 require 'selenium-webdriver'
 require 'rspec'
 require 'bundler'
-require 'rspec_html_reporter'
+#require 'rspec_html_reporter'
 
 
 RSpec.configure do |config|
-  config.default_formatter = 'RspecHtmlFormatter'
+  #config.default_formatter = 'RspecHtmlFormatter'
   
   config.before(:each) do
-    caps = Selenium::WebDriver::Remote::Capabilities.chrome(
-        :platformName => "linux",
-    )
-    @driver = Selenium::WebDriver.for(:remote, :url => "http://20.244.6.182:4444/wd/hub", :capabilities => caps)
+    option = Selenium::WebDriver::Chrome::Options.new
+    option.add_argument("--disable-gpu")
+  	option.add_argument("--disable-extensions")
+  	option.add_argument("--disable-infobars")
+  	option.add_argument("--start-maximized")
+  	option.add_argument("--disable-notifications")
+  	option.add_argument('--no-sandbox')
+  	option.add_argument('--disable-dev-shm-usage')
+    option.add_argument('disable-infobars')
+    option.add_argument("--remote-allow-origins=*")
+    option.page_load_strategy = :normal
+
+    @driver = Selenium::WebDriver.for(:remote, :url => "http://20.244.6.182:4444/wd/hub", :capabilities => option)
     @driver.navigate.to "http://leaftaps.com/opentaps/control/main"
 
     @driver.find_element(:id, "username").send_keys("demosalesManager")
